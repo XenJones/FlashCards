f1a5ca3079ec2afe071ea84118e0c568077c6e03
jamster3000 <jrbaines.04@gmail.com>
2024-10-15 12:24:13 +0100

    Updated data structure

---
+++ flashCards/Models/FlashCardPack.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace flashCards.Models
+{
+    public class FlashCardPack
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string PackName { get; set; }
+        public List<Flashcard> Flashcards { get; set; } = new List<Flashcard>();
+    }
+}
